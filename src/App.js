16dc2dbe5370f4d96837931ec9d07f5047016591
Koen van Gilst <koen.vangilst@gmail.com>
2018-06-10 23:49:27 +0200

    make the grid 5x5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import 'wired-elements'
 import './App.css'
 import Grid from './components/grid/'
 
@@ -10,8 +9,8 @@ class App extends React.Component {
   render () {
     return (
       <div className='app'>
-        <p>NORA</p>
-        <Grid size={4} solution={SOLUTION} noise={NOISE} />
+        <p>{SOLUTION.join('')}</p>
+        <Grid size={5} solution={SOLUTION} noise={NOISE} />
       </div>
     )
   }

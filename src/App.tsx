11c2a202022aeb30f91f1a4d2066fbda5048179d
Koen van Gilst <koen.vangilst@gmail.com>
2018-06-13 17:55:12 +0200

    parse name parameter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
+import * as qs from 'qs'
 import * as React from 'react'
 import './App.css'
 import Game from './components/Game/'
 
-const SOLUTION = 'NORA'.split('')
+const query = qs.parse(location.search.substr(1).toLowerCase())
+const SOLUTION = (query.name || 'NORA').toUpperCase()
 const NOISE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
 
 class App extends React.Component {
   public render() {
     return (
       <div className="app">
-        <p>{SOLUTION.join('')}</p>
-        <Game size={5} solution={SOLUTION} noise={NOISE} />
+        <p>{SOLUTION}</p>
+        <Game size={5} solution={SOLUTION.split('')} noise={NOISE} />
       </div>
     )
   }

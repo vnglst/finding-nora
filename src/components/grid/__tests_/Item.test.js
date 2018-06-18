078d768242682a63be196cbd370e02da2a6af0b8
Koen van Gilst <koen.vangilst@gmail.com>
2018-06-18 16:39:47 +0200

    fix strange error message

--- src/components/grid/__tests_/Item.test.js
+++ src/components/grid/__tests_/Item.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Item from '../Item'
+import Item from '../Item.tsx'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')

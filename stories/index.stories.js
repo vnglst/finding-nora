695b13a7b5b8c75d5fc01b9f0966b0d3c684c2e0
Koen van Gilst <koen.vangilst@gmail.com>
2018-07-01 20:30:10 +0200

    adds storybook stories for Overlay, Grid and BottomBar

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -4,7 +4,16 @@ import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { linkTo } from '@storybook/addon-links'
 
+import HomeIcon from '@material-ui/icons/Home'
+import RefreshIcon from '@material-ui/icons/Refresh'
+import SettingsIcon from '@material-ui/icons/Settings'
+
 import Button from 'components/UI/Button'
+import BottomBar from 'components/UI/BottomBar'
+import Grid from 'components/UI/Grid'
+import Overlay from 'components/UI/Overlay'
+
+import 'index.css'
 
 storiesOf('Button', module)
   .add('with text', () => (
@@ -17,3 +26,53 @@ storiesOf('Button', module)
       </span>
     </Button>
   ))
+
+storiesOf('BottomBar', module).add('with Home, Refresh and Settings', () => (
+  <BottomBar value="home" onChange={action('menu item changed')}>
+    <BottomBar.Item value="home" icon={<HomeIcon />} />
+    <BottomBar.Item value="new-game" icon={<RefreshIcon />} />
+    <BottomBar.Item value="settings" icon={<SettingsIcon />} />
+  </BottomBar>
+))
+
+storiesOf('Grid', module).add('with 3 items ', () => (
+  <Grid>
+    <Grid.Item
+      key="some-key"
+      onMouseDown={action('pressed')}
+      onTouchStart={action('touched')}
+      wrong={false}
+      correct={true}
+      almostCorrect={false}
+    >
+      1
+    </Grid.Item>
+    <Grid.Item
+      key="some-key-2"
+      onMouseDown={action('pressed')}
+      onTouchStart={action('touched')}
+      wrong={true}
+      correct={false}
+      almostCorrect={false}
+    >
+      2
+    </Grid.Item>
+    <Grid.Item
+      key="some-key-3"
+      onMouseDown={action('pressed')}
+      onTouchStart={action('touched')}
+      wrong={false}
+      correct={false}
+      almostCorrect={true}
+    >
+      3
+    </Grid.Item>
+  </Grid>
+))
+
+storiesOf('Overlay', module).add('with button', () => (
+  <Overlay>
+    <p>YOU LOST</p>
+    <Button onMouseDown={action('restart')}>Play again?</Button>
+  </Overlay>
+))

6c77b776253372047dcd85eea6db75fcb3de3023
Koen van Gilst <koen.vangilst@gmail.com>
2019-03-10 20:52:18 +0100

    feature(bugsnag) - add bugsnag integration, add sourcemaps

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
+import bugsnag from '@bugsnag/js'
+import bugsnagReact from '@bugsnag/plugin-react'
 import { Router } from '@reach/router'
-import FindingColors from 'src/finding-colors'
-import FindingWords from 'src/finding-words'
-import { audioMiddleware } from 'src/finding-words/redux/audio-middleware'
-import { IStoreState } from 'src/finding-words/types'
 import preventDoubleTapZoom from 'prevent-double-tap-zoom'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { applyMiddleware, compose, createStore } from 'redux'
+
+import FindingColors from 'src/finding-colors'
+import FindingWords from 'src/finding-words'
+import { audioMiddleware } from 'src/finding-words/redux/audio-middleware'
+import { IStoreState } from 'src/finding-words/types'
 import rootReducer from 'src/shared/redux/root-reducer'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
@@ -22,13 +25,19 @@ const store = createStore<IStoreState, any, any, any>(
   composeEnhancers(applyMiddleware(audioMiddleware))
 )
 
+const bugsnagClient = bugsnag('eb9c66e47f7f95c5801a21ffe1308619')
+bugsnagClient.use(bugsnagReact, React)
+const ErrorBoundary = bugsnagClient.getPlugin('react')
+
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <FindingWords path="/" default={true} />
-      <FindingColors path="/finding-colors" />
-    </Router>
-  </Provider>,
+  <ErrorBoundary>
+    <Provider store={store}>
+      <Router>
+        <FindingWords path="/" default={true} />
+        <FindingColors path="/finding-colors" />
+      </Router>
+    </Provider>
+  </ErrorBoundary>,
   document.getElementById('root') as HTMLElement
 )
 registerServiceWorker()

fc3e934a72a471a12725479a7f150f7cf23a4b60
Koen van Gilst <vnglst@users.noreply.github.com>
2019-03-11 11:51:34 +0100

    fix(analytics) - no tracking when on development (#182)

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,9 @@
-import bugsnag from '@bugsnag/js'
-import bugsnagReact from '@bugsnag/plugin-react'
 import { Router } from '@reach/router'
 import preventDoubleTapZoom from 'prevent-double-tap-zoom'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { applyMiddleware, compose, createStore } from 'redux'
-
 import FindingColors from 'src/finding-colors'
 import FindingWords from 'src/finding-words'
 import { audioMiddleware } from 'src/finding-words/redux/audio-middleware'
@@ -14,8 +11,8 @@ import { IStoreState } from 'src/finding-words/types'
 import rootReducer from 'src/shared/redux/root-reducer'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
-
-preventDoubleTapZoom({ delay: 500 })
+import { initializeAnalytics } from './shared/utils/analytics'
+import { BugsnagErrorBoundary } from './shared/utils/bugsnag'
 
 const composeEnhancers =
   (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
@@ -25,19 +22,18 @@ const store = createStore<IStoreState, any, any, any>(
   composeEnhancers(applyMiddleware(audioMiddleware))
 )
 
-const bugsnagClient = bugsnag('eb9c66e47f7f95c5801a21ffe1308619')
-bugsnagClient.use(bugsnagReact, React)
-const ErrorBoundary = bugsnagClient.getPlugin('react')
-
 ReactDOM.render(
-  <ErrorBoundary>
+  <BugsnagErrorBoundary>
     <Provider store={store}>
       <Router>
         <FindingWords path="/" default={true} />
         <FindingColors path="/colors" />
       </Router>
     </Provider>
-  </ErrorBoundary>,
+  </BugsnagErrorBoundary>,
   document.getElementById('root') as HTMLElement
 )
+
+preventDoubleTapZoom({ delay: 500 })
 registerServiceWorker()
+initializeAnalytics()

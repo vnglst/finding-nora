15c82a36797823f6cc58eb3040a8d285bbeb1917
Koen van Gilst <koen.vangilst@gmail.com>
2019-03-16 21:45:07 +0100

    chore(sw) - disable serviceworkers for now

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ import { audioMiddleware } from 'src/finding-words/redux/audio-middleware'
 import { IStoreState } from 'src/finding-words/types'
 import rootReducer from 'src/shared/redux/root-reducer'
 import './index.css'
-import registerServiceWorker from './registerServiceWorker'
+import { unregister } from './registerServiceWorker'
 import { BugsnagErrorBoundary } from './shared/utils/bugsnag'
 
 const composeEnhancers =
@@ -35,7 +35,7 @@ ReactDOM.render(
 )
 
 preventDoubleTapZoom({ delay: 500 })
-registerServiceWorker()
+unregister()
 
 function initializeAnalyticsOnProduction() {
   if (process.env.NODE_ENV === 'production') {
